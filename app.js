f411b78bcf5852a7c4dd891f83e0a9231bb00ee6
Francois MAESTRATI <francois.maestrati@gmail.com>
2020-09-27 17:51:59 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+Création d'un global component (affiche un Date.now )
+https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date/now
+Création d'un local component hello (affiche hello world)
+*/
+
+var vm = new Vue({
+  el: "#app",
+});
